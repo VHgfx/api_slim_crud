7c9f717c91448afffd4b993886d59951f660de80
VHgfx <hu-victor@hotmail.com>
2025-02-03 10:30:35 +0100

    Modification loadEnv, prise en charge .env.local/dev/prod

--- config/loadEnv.php
+++ config/loadEnv.php
@@ -1,4 +1,5 @@
-<?php // /config/loadEnv.php
+<?php
+# /config/loadEnv.php
 
 function loadEnv($filePath) {
     if (!file_exists($filePath)) {
@@ -8,21 +9,32 @@ function loadEnv($filePath) {
     $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
     foreach ($lines as $line) {
-        // # Pour commenter
         if (strpos(trim($line), '#') === 0) {
             continue;
         }
 
-        // Split la ligne en key / value
         list($key, $value) = explode('=', $line, 2);
 
         $key = trim($key);
         $value = trim($value);
 
-        // Définition de la variable d'environnement
         putenv("$key=$value");
         $_ENV[$key] = $value;
     }
 }
 
-loadEnv(__DIR__ . '/../.env');
+loadEnv(__DIR__ . '/.env');
+
+// Determine environement (e.g., 'local', 'dev', 'prod')
+$env = getenv('APP_ENV') ?: 'dev';
+
+$envFile = __DIR__ . "/.env.$env";
+if (file_exists($envFile)) {
+    loadEnv($envFile);
+}
+
+// Facultatif : Si .env.local existe, override tout le reste
+$localEnvFile = __DIR__ . '/../.env.local';
+if (file_exists($localEnvFile)) {
+    loadEnv($localEnvFile);
+}
